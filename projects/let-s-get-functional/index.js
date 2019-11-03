3ed8ae18c1662f9acce04559835e5835e8deb34f
Ubuntu <ubuntu@ip-172-31-36-16.ec2.internal>
2019-11-03 00:40:17 +0000

    11/2/19

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -23,9 +23,9 @@ var _ = require("lodown-benlmeyers");
 
 var maleCount = function(array) {
     //must use filter function
-   var malesarr = _.filter(array, function(customerObj, i, array) {
+    var malesarr = _.filter(array, function(customerObj, i, array) {
         return customerObj.gender === "male";
-    }); 
+    });
     return malesarr.length;
 };
 
@@ -33,51 +33,53 @@ var femaleCount = function(array) {
     //use reduce function
     //input: array
     //output: number
-   return _.reduce(array, function(seed, customerObj, i, arr) {
-      //if customerObj's gender is female add 1
-      if(customerObj.gender === "female") {
-          seed++;
-      } 
-      return seed;
-   }, 0);
+    return _.reduce(array, function(seed, customerObj, i, arr) {
+        //if customerObj's gender is female add 1
+        if (customerObj.gender === "female") {
+            seed++;
+        }
+        return seed;
+    }, 0);
 };
 
-var oldestCustomer = function(array) { 
+var oldestCustomer = function(array) {
     //find the oldest customer's name
     //input: array of customers
     //output: string of oldest customer's name
     //iterate through the array, and compare customer's ages
     //using reduce, compare current customer's age to previous customer's age
     //reassign the older one to the previous customer
-    
+
     return _.reduce(array, function(previous, current, i) {
-        if(current.age > previous.age) {
+        if (current.age > previous.age) {
             return current;
-        } else {
+        }
+        else {
             return previous;
         }
     }).name;
 };
 
 var youngestCustomer = function(array) {
-//find the youngest customer's name
-//input: array of customers
-//output: array of customers
-//iterate through the array, and compare customer's ages, return the youngest one
+    //find the youngest customer's name
+    //input: array of customers
+    //output: array of customers
+    //iterate through the array, and compare customer's ages, return the youngest one
     return _.reduce(array, function(previous, current, i) {
-        if(current.age < previous.age) {
+        if (current.age < previous.age) {
             return current;
-        } else {
+        }
+        else {
             return previous;
         }
-        
+
     }).name;
 };
 
 var averageBalance = function(array) {
-//iterate through the array of customers, find their balances
-//use the reduce method to add all their balances together
-//divide by the total number of customers
+    //iterate through the array of customers, find their balances
+    //use the reduce method to add all their balances together
+    //divide by the total number of customers
     let money = 0;
     let avgBalance = _.reduce(array, function(seed, customer, i, array) {
         money = customer.balance.slice(1);
@@ -93,72 +95,99 @@ var firstLetterCount = function(array, letter) {
     //first letter
     return _.reduce(array, function(seed, customer, i, array) {
         if (customer.name[0].toLowerCase() === letter.toLowerCase()) {
-            seed ++;
+            seed++;
         }
         return seed;
     }, 0);
 };
 
 var friendFirstLetterCount = function(array, customer, letter) {
-//iterate through the array of customers, find the given customer
-//find the friends list, and iterate through that comparing the first letter of
-//their names to the given letter
-   let customerMatch = {};
-for(let i = 0; i < array.length; i++) {
-    if(array[i].name === customer){
-        customerMatch = array[i];
-        break;
+    //iterate through the array of customers, find the given customer
+    //find the friends list, and iterate through that comparing the first letter of
+    //their names to the given letter
+    let customerMatch = {};
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].name === customer) {
+            customerMatch = array[i];
+            break;
+        }
     }
-}  
-return  _.reduce(customerMatch.friends, function(seed, current, j, array) {
-        if(current.name[0].toLowerCase() === letter.toLowerCase()) {
+    return _.reduce(customerMatch.friends, function(seed, current, j, array) {
+        if (current.name[0].toLowerCase() === letter.toLowerCase()) {
             seed++;
         }
         return seed;
-        }, 0);
+    }, 0);
 };
 
 var friendsCount = function(array, name) {
-//Find the customers' names that have a given customer's name in their friends list
-//inputs: array of customers, name to be found
-//ouput: array of customers who have the given name in their friends list
-for (let i = 0; i < array.length; i++) {
-        _.reduce(array[i].friends, function(seed, current, j, array) {
-            if(array[i].friends[j] === name){
-                seed.push(name);
-            }
-        }, []);
-    }
+        //Find the customers' names that have a given customer's name in their friends list
+        //inputs: array of customers, name to be found
+        //ouput: array of customers who have the given name in their friends list
+        // for (let i = 0; i < array.length; i++) {
+        //         _.reduce(array[i].friends, function(seed, current, j, array) {
+        //             if(array[i].friends[j] === name){
+        //                 seed.push(name);
+        //             }
+        //         }, []);
+        //     }
+        };
+        var topThreeTags = function(array) {
+            // //Find the three most common tags among all customers' associated tags
+            // //array of customers
+            // //array of tags
+            // let TTtags = [];
+            // for(let i = 0; i < array.length; i++) {
+            //   for(let key in array[i]) {
+            //       TTtags.friends.push(array[i])
+            //   }
+            // }
+            // }
+            // console.log('TTtags', TTtags);
+            // let tagCount = _.reduce(array, function(acc, currentTag, i, array) {
+            //     if(acc[currentTag] === undefined){
+            //         acc[currentTag] = 1;
+            //   } else {
+            //     acc[currentTag]++;
+            //   }
+            //   return acc;
+            // }, {});
+            // console.log('tagCount', tagCount)
+            // let nestedArr = [];
+            //     _.each(tagCount, function(seed, tag, object){
+            //   nestedArr.push([tag, seed]);
+            // });      
+            //      console.log(nestedArr.sort(function(a, b){
+            //   return b[1] - a[1];
+            // }));
+            // var resultArr = [];
+            //     for(let i = 0; i < 3; i++){
+            //     resultArr.push(nestedArr[i][0]);
+            };
+
+
+
+        var genderCount = function(array) {
+            //create a summary of genders
+            //input: array of customers
+            //output: object with genders as keys, and numbers as values
 };
-var topThreeTags = function(array) {
-//Find the three most common tags among all customers' associated tags
-//array of customers
-//array of tags
 
 
-};
-
-var genderCount = function(array) {
-//create a summary of genders
-//input: array of customers
-//output: object with genders as keys, and numbers as values
 
 
-    
-};
+        //////////////////////////////////////////////////////////////////////
+        // DON'T REMOVE THIS CODE ////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////
-// DON'T REMOVE THIS CODE ////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-// here, export any references you need for tests //
-module.exports.maleCount = maleCount;
-module.exports.femaleCount = femaleCount;
-module.exports.oldestCustomer = oldestCustomer;
-module.exports.youngestCustomer = youngestCustomer;
-module.exports.averageBalance = averageBalance;
-module.exports.firstLetterCount = firstLetterCount;
-module.exports.friendFirstLetterCount = friendFirstLetterCount;
-module.exports.friendsCount = friendsCount;
-module.exports.topThreeTags = topThreeTags;
-module.exports.genderCount = genderCount;
+        // here, export any references you need for tests //
+        module.exports.maleCount = maleCount;
+        module.exports.femaleCount = femaleCount;
+        module.exports.oldestCustomer = oldestCustomer;
+        module.exports.youngestCustomer = youngestCustomer;
+        module.exports.averageBalance = averageBalance;
+        module.exports.firstLetterCount = firstLetterCount;
+        module.exports.friendFirstLetterCount = friendFirstLetterCount;
+        module.exports.friendsCount = friendsCount;
+        module.exports.topThreeTags = topThreeTags;
+        module.exports.genderCount = genderCount;
